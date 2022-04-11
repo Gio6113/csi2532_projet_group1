5ffe800fffe97ba65443d7f78b8823e94b4a140e
Giorgio Sawaya <giorgiosawaya@gmail.com>
2022-04-11 01:25:54 -0400

    changes to seed.sql

--- Seed.sql
+++ Seed.sql
@@ -1,5 +1,6 @@
 SET search_path =  public;
 INSERT INTO usr_user 
+(user_id, first_name, last_name, full_name, username, password,user_type,dob,age)
 VALUES
      --Original Admins	
 	(1,'Giorgio','Sawaya','Giorgio Sawaya','gio123','weakpassword11','admin','1991-01-09',31),
@@ -29,14 +30,38 @@ VALUES
 	(23,'Maxime','Fortier','Maxime Fortier','maxFort67','maxFort67','casual','1979-02-20',43),
 	(24,'Fatima','Bladi','Fatima Bladi','famfam33','famfam33','casual','1978-02-21',44),
 	(25,'Latisha','Armour','Latisha Armour','ArmourLati22','ArmourLati22','casual','2008-02-22',14),
+	(26,'Mariam','Mendy','Mariam Mendy','marmendy66','marmendy66','casual','1979-02-26',43),
+	(27,'Brandon','Smith','Brandon Smith','bransmithsy22','bransmithsy22','casual','1978-02-25',44),
 	--Original doctors/medical staff
-	(26,'Christine','Aguilera','Christine Aguilera','voice22','voice22','employee','1976-02-23',46),
-	(27,'Jean','Lafleur','Jean Lafleur','jean2dentist','jean2dentist','employee','1975-02-24',47),
-	(28,'Martha','Poliski','Martha Poliski','polisher1','polisher1','employee','1974-02-19',48),
-	(29,'Emmanuel','Macaron','Emmanuel Macaron','GiletJaune12','GiletJaune12','employee','1973-02-08',49),
-	(30,'Mia','Turner','Mia Turner','worker4ever','worker4ever','employee','1962-02-09',60),
-	(31,'Michael','Godfrey','Michael Godfrey','Miketooth24','Miketooth24','employee','1952-02-22',70),
-	(32,'Zinedine','Zidane','Zinedine Zidane','bluetooth13','siuuu22','employee','1950-07-13',71)
+	(28,'Christine','Aguilera','Christine Aguilera','voice22','voice22','employee','1976-02-23',46),
+	(29,'Jean','Lafleur','Jean Lafleur','jean2dentist','jean2dentist','employee','1975-02-24',47),
+	(30,'Martha','Poliski','Martha Poliski','polisher1','polisher1','employee','1974-02-19',48),
+	(31,'Emmanuel','Macaron','Emmanuel Macaron','GiletJaune12','GiletJaune12','employee','1973-02-08',49),
+	(32,'Mia','Turner','Mia Turner','worker4ever','worker4ever','employee','1962-02-09',60),
+	(33,'Michael','Godfrey','Michael Godfrey','Miketooth24','Miketooth24','employee','1952-02-22',70),
+	(34,'Zinedine','Zidane','Zinedine Zidane','bluetooth13','siuuu22','employee','1950-07-13',71),
+	(35,'Jacob','Versace','Jacob Versace','jacobEmpAccount','$1234$','employee','1995-02-01',27)
 ;
 
+
+INSERT INTO usr_patient 
+(patient_id, ssn, address, phone, email, insurance_type,responsible_id,user_id,gender, is_currently_employee,employee_id)
+VALUES
+	(1,234998123,'45 Duney Street, Gatineau, QC',		'343-555-2345', 'JeffSulivan@gmail.com',		'No current insurance plan'		,NULL,	6,	'Male',		false,NULL),
+	(2,456770990,'543 Queen Street, Regina, SK',		'306-132-3144',	'DalilaDesktop@hotmail.com',	'No current insurance plan'		,NULL,	7,	'Female',	false,NULL),
+	(3,943353674,'244 Portland Drive, Toronto, ON',		'416-765-3452',	'MariaKafi@deezer.com',			'No current insurance plan'		,NULL,	8,	'Female',	false,NULL),
+	(4,913225741,'65 Torres Road, Toronto, ON',			'648-233-9870',	'JohnBond@yahoo.com',			'CAA Full Insurance'			,NULL,	9,	'Male',		false,NULL),
+	(5,245570079,'2331 Palais Rue, Quebec City, QC',	'623-841-3321',	'FredericLeclair@icloud.com',	'CAA Full Insurance'			,NULL,	10,	'Male',		false,NULL),
+	(6,129453654,'12 Golf Street, Toronto, ON',			'416-432-6555',	'SamanthaWond@gmail.com',		'CAA Partial Insurance'			,NULL,	11,	'Female',	false,NULL),
+	(7,843858678,'36 Bailey Avenue, Vancouver, BC',		'514-565-0031',	'SusanSingh@outlook.com',		'CAA Partial Insurance'			,NULL,	12,	'Female',	false,NULL),
+	(8,543100688,'100 Darkoak Promenade, Montreal, QC',	'438-544-7129',	'LucyHue@me.com',				'Greenshield Full Insurance'	,NULL,	13,	'Non-Binary',false, NULL),
+	(9,453383970,'201 Popular Street, Gatineau, QC',	'343-223-3267',	'JenniferLopex@icloud.com',		'Greenshield Full Insurance'	,NULL,	14,	'Female',	false,NULL),
+	--(10,99236666,'1921 Designer Avenue, Winnipec, MB',	'533-234-8874',	'JacobVersace@gucci.com',		'Employee Benefit Insurance'	,NULL,	15,	'Male',		true , 8),
+	(11,96373765,'212 Maverick Street, Halifax, NS',	'452-905-8447',	'YoussefBenArfa@yahoo.com',		'Greenshield Student Insurance'	,NULL,	16,	'Male',		false,NULL),
+	(12,23326784,'882 Mully Drive, Ottawa, ON',			'613-443-4664',	'WilliamBlack@gmail.com',		'Greenshield Student Insurance'	,NULL,	17,	'Male',		false,NULL),
+	(13,92346567,'12 Rideau Street, Ottawa, ON',		'613-841-5431',	'WendyMendy@gmail.com',			'SureHealth Youth Insurance'	,26,	18,	'Female',	false,NULL),
+	(14,93578531,'3131 Labelle Promenade, Montreal, QC','438-542-9403',	'DonnaSmith@outlook.com',		'SureHealth Youth Insurance'	,27,	19,	'Female',	false,NULL),
+	(15,14335366,'444 Didier Road, Toronto, ON',		'648-950-2534',	'DonovanDrogba@yahoo.com',		'Greenshield Youth Insurance'	,22,	20,	'Male',		false,NULL),
+	(16,57853971,'241 Visenaut Avenue, Fredericton, NB','765-904-4456',	'DaveDuncaster@gmail.com',		'SureHealth Youth Insurance'	,23,    21,	'Male',		false,NULL)
+;																																								
 
