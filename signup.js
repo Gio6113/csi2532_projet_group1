c656629e84a66014ae4ff33132a9f8ced7ebff05
Pierre <pierreakladios@gmail.com>
2022-04-18 17:45:42 -0400

    signup improvement

---
+++ signup.js
@@ -0,0 +1,22 @@
+function twoReceptionist(location){
+    return true;
+}
+
+function isOldEnough(){
+    var x = document.getElementById("dob");
+    x
+    return true;
+}
+
+function showExtraFields(divName){
+    var x = document.getElementById(divName);
+    if(x.value>="2007-12-31"){
+        showExtraFields(underage);
+    }
+}
+
+function hideExtraFields(divName){  
+    var x = document.getElementById(divName);
+    x.setAttribute("hidden");
+}
+
